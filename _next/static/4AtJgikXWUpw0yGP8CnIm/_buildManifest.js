70b93ef5038159bd9c801dc8433f581ef4c1ec44
jayyap <>
2023-07-21 05:43:43 +0000

    Updates

---
+++ _next/static/4AtJgikXWUpw0yGP8CnIm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c771aa87cc92e555.css","static/chunks/pages/index-cf979bc676b06143.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
